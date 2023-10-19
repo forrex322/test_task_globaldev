931476dbf0f45672e065b8e1c5224dc6cb2c2606
andrieiev.dmytro <andrieiev.dmytro@steelkiwi.com>
2023-10-19 14:27:58 +0300

    initial commit

---
+++ config/settings.py
@@ -0,0 +1,167 @@
+import sys
+import environ
+
+# globaldev_app/config/settings.py - 2 = globaldev_app/
+ROOT_DIR = environ.Path(__file__) - 2
+APPS_DIR = ROOT_DIR.path("globaldev_app")
+sys.path.append("globaldev_app/apps")
+
+# Environment
+# https://django-environ.readthedocs.io/en/latest/#how-to-use
+# ------------------------------------------------------------------------------
+env = environ.Env(
+    DJANGO_DEBUG=(bool, False),
+    DJANGO_SECRET_KEY=(str, ""),
+    DJANGO_ADMINS=(list, []),
+    DJANGO_ALLOWED_HOSTS=(list, []),
+    # Static/Media
+    DJANGO_STATIC_ROOT=(str, str(APPS_DIR("staticfiles"))),
+    DJANGO_MEDIA_ROOT=(str, str(APPS_DIR("media"))),
+    # Debug
+    DJANGO_USE_DEBUG_TOOLBAR=(bool, False),
+    DJANGO_TEST_RUN=(bool, False),
+    DJANGO_DEBUG_SQL=(bool, False),
+    DJANGO_DEBUG_SQL_COLOR=(bool, False),
+    # CORS
+    DJANGO_CORS_ORIGIN_WHITELIST=(list, []),
+    DJANGO_CORS_ALLOW_HEADERS=(tuple, ()),
+    DJANGO_CORS_ORIGIN_ALLOW_ALL=(bool, False),
+    # Database
+    POSTGRES_HOST=(str, "db"),
+    POSTGRES_PORT=(int, 5432),
+    POSTGRES_DB=(str, ""),
+    POSTGRES_USER=(str, ""),
+    POSTGRES_PASSWORD=(str, ""),
+    # Celery
+    DJANGO_CELERY_BROKER_URL=(str, "redis://redis:6379/0"),
+    DJANGO_CELERY_BACKEND=(str, "redis://redis:6379/0"),
+    DJANGO_CELERY_TASK_ALWAYS_EAGER=(bool, False),
+)
+
+# Django Core
+# https://docs.djangoproject.com/en/2.2/ref/settings/#core-settings
+DEBUG = env.bool("DJANGO_DEBUG")
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+# SECRET_KEY = 'django-insecure-(yo8h$q97e7jo-^)qj#3n^3p#ckj#=ms(7m-i2%6@om0n603z9'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+ROOT_URLCONF = "config.urls"
+WSGI_APPLICATION = "config.wsgi.application"
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+STATIC_URL = "/static/"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+ADMIN_SITE_TITLE = "globaldev_app"
+ADMIN_SITE_HEADER = "globaldev_app"
+ADMINS = tuple([tuple(admins.split(":")) for admins in env.list("DJANGO_ADMINS")])
+MANAGERS = ADMINS
+ADMIN_URL = "admin/"
+
+# Django Sites
+# https://docs.djangoproject.com/en/2.2/ref/settings/#sites
+# ------------------------------------------------------------------------------
+SITE_ID = 1
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
+
+# Django Applications
+# https://docs.djangoproject.com/en/2.2/ref/settings/#installed-apps
+# ------------------------------------------------------------------------------
+DJANGO_APPS = (
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+)
+THIRD_PARTY_APPS = (
+    "rest_framework",
+    "drf_yasg",
+)
+LOCAL_APPS = (
+    "users.apps.UsersConfig",
+    "taskapp.apps.TaskAppConfig",
+    "booktimetracker.apps.BooktimetrackerConfig",
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# Django Static Files
+# https://docs.djangoproject.com/en/2.2/ref/settings/#static-files
+# ------------------------------------------------------------------------------
+STATIC_URL = "/static/"
+STATIC_ROOT = env("DJANGO_STATIC_ROOT")
+STATICFILES_DIRS = (str(APPS_DIR.path("static")),)
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
+
+# Celery
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html
+# ------------------------------------------------------------------------------
+CELERY_BROKER_URL = env("DJANGO_CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = env("DJANGO_CELERY_BACKEND")
+if env.bool("DJANGO_TEST_RUN") or any("pytest" in arg for arg in sys.argv):
+    CELERY_TASK_ALWAYS_EAGER = True
+else:
+    CELERY_TASK_ALWAYS_EAGER = env.bool("DJANGO_CELERY_TASK_ALWAYS_EAGER")
---
+++ config/urls.py
@@ -0,0 +1,61 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+
+# Swagger configs
+schema_view = get_schema_view(
+    openapi.Info(
+        title=settings.ADMIN_SITE_TITLE,
+        default_version="v1",
+    ),
+    validators=[],
+    public=True,
+    permission_classes=(AllowAny,),
+    authentication_classes=(),
+)
+
+urlpatterns = [
+    url(r"^i18n/", include("django.conf.urls.i18n")),
+]
+
+urlpatterns += i18n_patterns(
+    # drf-yasg url
+    re_path(
+        r"^docs/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    # API urls
+    path(
+        "api/v1/",
+        include(
+            (
+                [
+                    path("", include(("users.urls", "users"))),
+                    path("", include(("booktimetracker.urls", "booktimetracker"))),
+                ],
+                "api",
+            ),
+            namespace="api",
+        ),
+    ),
+    prefix_default_language=False,
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+
+# Admin Site Config
+# https://docs.djangoproject.com/en/2.2/ref/contrib/admin/#adminsite-attributes
+admin.sites.AdminSite.site_header = settings.ADMIN_SITE_HEADER
+admin.sites.AdminSite.site_title = settings.ADMIN_SITE_TITLE
